7814665a964039465851232c24433b9b0a5fda3a
Himanshu Vohra <hvohra812@gmail.com>
2016-11-02 18:20:07 +0530

    Finding number of strongly connected components.
    A directed graph is strongly connected if there is a path between all pairs of vertices. A strongly connected component (SCC) of a directed graph is a maximal strongly connected subgraph. 
    Complexity : O(V+E)
    Graph is represented using Adjacency List

--- Graph/StronglyConnectedComponent.java
+++ Graph/StronglyConnectedComponent.java
@@ -1,10 +1,6 @@
 import java.util.*;
 
-/*A directed graph is strongly connected if there is a path between 
-all pairs of vertices. A strongly connected component (SCC) of a 
-directed graph is a maximal strongly connected subgraph.
-Complexity : O(V+E)
-Graph is represented using Adjacency List*/
+/**/
 
 class StronglyConnectedComponent
 {
